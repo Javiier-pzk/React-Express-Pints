fbcca93d8516797814cbb76712823d9d71e1dc59
Javier <javierphon@gmail.com>
2023-01-25 23:26:53 +0800

    Add frontend logic to communicate with backend server

--- Client/App.js
+++ Client/App.js
@@ -1,7 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const SERVER_URL= `http://localhost:8080`  
+  const symbols = ['aapl', 'nflx', 'goog', 'amzn', 'tsla']
+  const [isLoading, setIsLoading] = useState(true)
+  const data = []
+  
+  useEffect(() => {
+    const getData= async () => {
+      for (let symbol of symbols) {
+        const response = fetch(`${SERVER_URL}/?symbol=${symbol}`)
+        const d = await (await response).json()
+        data.push(d)
+      }
+    }
+
+    getData()
+      .then(() => setIsLoading(false))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
