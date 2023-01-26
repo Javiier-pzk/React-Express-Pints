08de7121d2751d2fe742a54c48e3a0a9efe4edf5
Javier <javierphon@gmail.com>
2023-01-27 00:53:00 +0800

    Build front end design and list view

--- Client/App.js
+++ Client/App.js
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, ActivityIndicator, SafeAreaView, SectionList, FlatList } from 'react-native';
-import { List } from 'react-native-paper';
+import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
+import { Avatar, List, Provider as PaperProvider } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { AntDesign } from '@expo/vector-icons'; 
+
 
 export default function App() {
   const SERVER_URL= `http://localhost:8080`  
@@ -21,43 +24,79 @@ export default function App() {
 
     getData()
       .then(() => {
-        console.log('datatemp', dataTemp)
         setData(dataTemp)
-        console.log('data', data)
         setIsLoading(false)
       })
       .catch(err => console.log(err))
   }, [])
 
-  const renderItems = ({item}) => {
-    return (
+  const renderList = ({ item }) => (
       <List.Item
+        key={item.key}
         style = {styles.item}
-        title = {item.symbol}
+        title = {item.key}
+        titleNumberOfLines = {1}
+        descriptionNumberOfLines = {1}
         description = {item.companyName}
         titleStyle = {styles.title}
         descriptionStyle = {styles.description}
+        left = {(props) => renderImage(props, item.companyLogo)}
+        right = {(props) => renderPreviousPrices(props, item)}
       />
-    )
-  }
+  )
+
+  const renderImage = (props, imgUrl) => (
+    <Avatar.Image 
+      {...props} 
+      size = {55}
+      source = {{uri: imgUrl}}
+    />
+  )
+
+  const renderPreviousPrices = (props, item) => (
+    <View {...props} style = {styles.prices}>
+      <Text style = {styles.latestPrice}>${item.latestPrice}</Text>
+      <View style = {styles.previousPrices}>
+        <View style = {styles.priceChange}>
+          {item.change >= 0 
+            ? (<AntDesign name="arrowup" size = {16} color = 'green'/>)
+            : (<AntDesign name="arrowdown" size = {16} color = 'red' />)
+          }
+          <Text style = {
+              item.change >= 0 
+                ? styles.increaseChange
+                : styles.decreaseChange
+            }>{item.change}
+          </Text>
+        </View>
+        <Text style = {
+            item.changePercent >= 0 
+              ? styles.increaseChange
+              : styles.decreaseChange
+          }> {item.changePercent}%
+        </Text>
+      </View>
+    </View>
+  )
 
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      {isLoading ? (
-        <View style = {styles.loadingView}>
-          <ActivityIndicator size='large' style={styles.loading} />
-          <Text>Fetching stock info...</Text>
-        </View>
-      ) : (
-        <View style={{flex: 1}}>
+    <PaperProvider>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="auto" />
+        {isLoading ? (
+          <View style = {styles.loadingView}>
+            <ActivityIndicator size='large' style={styles.loading} />
+            <Text>Fetching stock info...</Text>
+          </View>
+        ) : (
           <FlatList
-            sections = {data}
-            renderItem = {renderItems}
+            style = {styles.list}
+            data = {data}
+            renderItem = {renderList}
           />
-        </View>
-      )}
-    </SafeAreaView>
+        )}
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
@@ -70,19 +109,17 @@ const styles = StyleSheet.create({
     padding: 20
   },
   title: {
-    alignSelf: "center",
     fontSize: 18,
     fontWeight: "bold",
     marginBottom: 5,
   },
   description: {
-    alignSelf: "center",
     fontSize: 16,
   },
   item: {
     paddingTop: 20,
     paddingBottom: 20,
-    borderBottomWidth: 0.4,
+    borderBottomWidth: 1,
     borderBottomColor: "#999",
   },
   loadingView: {
@@ -92,5 +129,35 @@ const styles = StyleSheet.create({
   loading: {
     marginBottom: 10,
     color: '#4682b4'
+  },
+  list: {
+    flex: 1,
+    marginTop: 20,
+    width: "100%"
+  },
+  prices: {
+    justifyContent: 'center'
+  },
+  latestPrice: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    alignSelf: 'center'
+  },
+  previousPrices: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignSelf: 'center'
+  },
+  priceChange: {
+    flexDirection: 'row',
+    marginRight: 5
+  },
+  decreaseChange: {
+    fontSize: 14,
+    color: 'red'
+  },
+  increaseChange: {
+    fontSize: 14,
+    color: 'green'
   }
 });
